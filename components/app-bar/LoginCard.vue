4ae8156b4bcc874620f99525558608417724f029
potatojoayo <potatojoayo@gmail.com>
2021-08-05 15:35:41 +0900

    LoginCard 스타일 변경

--- components/app-bar/LoginCard.vue
+++ components/app-bar/LoginCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="login-card">
     <v-card
       color="primary"
       width="300px"
@@ -18,10 +18,7 @@
             label="이메일"
             required
             dense
-            class="
-              secondary--text
-              login-text-field
-            "
+            class="login-text-field"
             color="secondary"
           />
           <v-text-field
@@ -55,7 +52,8 @@
               >로그인</v-btn
             >
           </v-row>
-          <span class="secondary--text"
+          <span
+            class="secondary--text ask"
             ><a>이메일 </a>혹은<a>
               비밀번호</a
             >를 잊으셨습니까?</span
@@ -77,45 +75,102 @@ export default Vue.extend({
   },
 })
 </script>
-<style lang='scss' scoped>
-.v-card {
-  border-radius: 0 3px 5px 5px !important;
-}
-.v-card__title {
-  font-family: 'BMHANNAPro';
-  padding: 10px 0 0 10px;
-}
-.v-text-field__slot {
-  color: red !important;
-}
-.row {
-  padding: 10px !important;
-}
-.v-btn {
-  padding: 0px 10px !important;
-  margin: 5px !important;
-  font-family: 'BMHANNAPro';
-  border-radius: 0 !important;
-}
-span {
-  font-family: 'BMHANNAPro';
-  display: block;
-  text-align: center;
-  margin-top: 12px;
-  a {
-    text-decoration: none;
-    color: #4eb9f5;
+<style lang='scss'>
+.login-card {
+  .v-card {
+    border-radius: 0 3px 5px 5px !important;
   }
-}
-.triangle {
-  width: 0px;
-  height: 0px;
-  position: absolute;
-  top: -14px;
+  .v-card__title {
+    font-family: 'BMHANNAPro';
+    padding: 10px 0 0 10px;
+  }
+  .row {
+    padding: 10px !important;
+  }
+  span {
+    font-family: 'BMHANNAPro';
+    display: block;
+    text-align: center;
+    a {
+      text-decoration: none;
+      color: #0096c7;
+    }
+  }
 
-  right: 0px;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 15px solid #363636;
+  button {
+    span {
+      font-size: 1rem;
+    }
+  }
+
+  .ask {
+    margin-top: 20px;
+  }
+  .triangle {
+    width: 0px;
+    height: 0px;
+    position: absolute;
+    top: -14px;
+
+    right: 0px;
+    border-left: 35px solid transparent;
+    border-right: 35px solid transparent;
+    border-bottom: 15px solid #363636;
+  }
+
+  .theme--dark .triangle {
+    border-bottom-color: #d8cfc7;
+  }
+
+  .login-text-field.theme--dark {
+    label {
+      color: #808080 !important;
+      font-family: 'BMHANNAPro';
+    }
+    input {
+      color: #363636 !important;
+      font-family: 'BMHANNAPro';
+    }
+    input[type='password'] {
+      color: #363636 !important;
+      font-family: 'Roboto';
+    }
+  }
+
+  .login-text-field {
+    .v-btn {
+      padding: 0px 10px !important;
+      margin: 5px !important;
+      font-family: 'BMHANNAPro';
+      border-radius: 0 !important;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+    }
+    label {
+      color: #b0b0b0 !important;
+      font-family: 'BMHANNAPro';
+      font-size: 1rem;
+    }
+    input {
+      color: #d8cfc7 !important;
+      font-family: 'BMHANNAPro';
+    }
+
+    .v-input__slot:active {
+      border-color: #b0b0b0 !important;
+    }
+    .v-input__slot::before {
+      border-color: #b0b0b0 !important;
+    }
+    .v-input__slot:hover {
+      border-color: #b0b0b0 !important;
+    }
+  }
+
+  input[type='password'] {
+    color: #d8cfc7 !important;
+    font-family: 'Roboto';
+  }
 }
 </style>
